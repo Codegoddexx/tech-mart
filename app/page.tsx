5c841870937ade06772fc76a1e5e6bd7fb256779
Precious Iwuala <129090765+Codegoddexx@users.noreply.github.com>
2026-02-17 06:04:17 +0000

    Complete hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,9 @@
-import { Button } from "@/components/ui/button";
-import { Badge } from "@/components/ui/badge"
+import HeroSection from "@/components/landing-page/hero-section";
 
 export default function Home() {
   return (
     <div>
-      <Badge>
-        Join thousands of creators sharing their work</Badge>
-      <h1>  Share What You've built, Discover What's Launching </h1>
-      <p>A community platform for creators to showcase their apps, AI tools, SaaS products, and creative projects. Authentic launches, real builders, genuine feedback.</p>
-      <Button>Share Your Project</Button>
-      <Button>Explore Project</Button>
+     <HeroSection/>
     </div>
   );
 }
