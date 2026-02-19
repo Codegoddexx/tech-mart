5629df5fd0e683a1d339bf60677c37490ca05543
Precious Iwuala <129090765+Codegoddexx@users.noreply.github.com>
2026-02-19 14:09:45 +0000

    Full landing page and database setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from '@clerk/nextjs'
+import Header from "@/components/common/header";
+import Footer from "@/components/common/footer";
+
 
 const outfit = Outfit({ subsets: ["latin"] });
 
@@ -8,17 +12,21 @@ export const metadata: Metadata = {
   title: "Share Your creations, Discover New Launches",
   description: "A community platform for creators to showcase their apps, AI tools, SaaS products, and creative projects. Authentic launches, real builders, genuine feedback.",
 };
- 
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <ClerkProvider>
+    <html lang="en">    
       <body>
+         <Header/>
         {children}
-      </body>
+         <Footer/>
+      </body>   
     </html>
+    </ClerkProvider>
   );
 }
